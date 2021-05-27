5e7934d3b948d72726597c26e08086885775533c
Jimmy <jimmy.c.aubert@gmail.com>
2021-05-27 16:25:30 +0200

    Exercice 4

--- test/src/app/app.component.ts
+++ test/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 import { Statistique } from './models/Statistiques';
 
 @Component({
@@ -6,7 +6,7 @@ import { Statistique } from './models/Statistiques';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'test';
 
   public stat1 = new Statistique(
@@ -20,7 +20,22 @@ export class AppComponent {
     "fa1f5f40-be3b-11eb-91ec-7f5875ecfb47",
     "Test",
     "6M",
+    "WARNING"
+  );
+
+  public stat3 = new Statistique(
+    "fa1f5f40-be3b-11eb-91ec-7f5875ecfb57",
+    "Test2",
+    "1M",
     "ERROR"
   );
 
+  public tabStat = [this.stat1,this.stat2];
+
+  ngOnInit(){
+    setTimeout(() => {
+      this.tabStat.push(this.stat3)
+    }, 3000);
+  }
+  
 }
